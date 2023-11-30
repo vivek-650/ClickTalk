e2a1141689517a63d94df3b2cf8357494394b068
Satendra kumar <satyendrakumar.contact@gmail.com>
2023-11-30 21:09:44 +0530

    begining

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("views engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs")
+})
+
+app.get("/create", (req, res) =>{
+    res.render("createroom.ejs")
+})
+
+app.listen(port, ()=>{
+    console.log(`Listening port ${port}`);
+})
